ee9001b017a28aeb04355374504ce3894bcb80b2
cececlar <cece@wyncode.co>
2021-04-04 11:06:44 -0400

    add nivo scatterplot for data visualization

--- client/src/components/TextData/TextData.jsx
+++ client/src/components/TextData/TextData.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { ResponsiveBullet } from "@nivo/bullet";
+import React, { useState, useEffect } from "react";
+import { ResponsiveScatterPlot } from "@nivo/scatterplot";
 import "./TextData.scss";
 
 export default function TextData({ userInput, data }) {
@@ -7,24 +7,83 @@ export default function TextData({ userInput, data }) {
   console.log(userInput);
   console.log(data);
 
+  useEffect(() => {
+    setBulletData([
+      {
+        id: `${userInput}`,
+        data: [
+          {
+            x: data[0].documentSentiment.score,
+            y: data[0].documentSentiment.magnitude,
+          },
+        ],
+      },
+    ]);
+  }, [data]);
+
   return (
     <>
-      {/* TODO: setBulletData & customize props for ResponsiveBullet chart
-    TODO: Resolve unused var warnings  */}
-      {/* {textData && (
+      <p>{userInput}</p>
+      {bulletData && (
         <section className="text-data-graph">
           {" "}
-          <ResponsiveBullet
+          <ResponsiveScatterPlot
             data={bulletData}
-            margin={{ top: 50, right: 90, bottom: 50, left: 90 }}
-            spacing={46}
-            titleAlign="start"
-            titleOffsetX={-70}
-            measureSize={0.2}
+            margin={{ top: 60, right: 140, bottom: 70, left: 90 }}
+            xScale={{ type: "linear", min: -1, max: 1 }}
+            xFormat={function (e) {
+              return e;
+            }}
+            yScale={{ type: "linear", min: -1, max: 1 }}
+            yFormat={function (e) {
+              return e;
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: "bottom",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "sentiment",
+              legendPosition: "middle",
+              legendOffset: 46,
+            }}
+            axisLeft={{
+              orient: "left",
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: "magnitude",
+              legendPosition: "middle",
+              legendOffset: -60,
+            }}
+            legends={[
+              {
+                anchor: "bottom-right",
+                direction: "column",
+                justify: false,
+                translateX: 130,
+                translateY: 0,
+                itemWidth: 100,
+                itemHeight: 12,
+                itemsSpacing: 5,
+                itemDirection: "left-to-right",
+                symbolSize: 12,
+                symbolShape: "circle",
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
           />
         </section>
-      )} */}
-      <p>{userInput}</p>
+      )}
     </>
   );
 }
